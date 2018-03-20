123c7e3fb82459406e302933dce61c3fba66a3a0
Rebecca Schrader <rmschrader1994@gmail.com>
2018-03-20 11:27:33 +0000

    heroku 3

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -5,9 +5,9 @@ DEBUG = True
 INSTALLED_APPS.append('storages')
 
 MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/" % (MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = "https://%s/%s/" % (STATICFILES_LOCATION)
