d4ae168d3c6a6522b162a28efe8440a68540d62c
Rebecca Schrader <rmschrader1994@gmail.com>
2018-03-20 11:02:21 +0000

    procfile added2

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-rmschrader1994.c9users.io']
+ALLOWED_HOSTS = ['ecommerce-rmschrader1994.c9users.io',
+                 'becca-ecommerce.herokuapp.com']
 
 
 # Application definition
