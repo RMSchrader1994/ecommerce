35820103981fafcfb5c3f4fd1741488061b5c322
Rebecca Schrader <rmschrader1994@gmail.com>
2018-03-22 15:55:28 +0000

    1

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .forms import OrderForm, MakePaymentForm
 from products.models import *
@@ -8,15 +8,20 @@ from cart.utils import get_cart_items
 
 # Create your views here.
 def checkout(request):
+
+    if request.method=="POST":
+        del request.session['cart']
+        return redirect("home")
     
-    order_form = OrderForm()
-    payment_form = MakePaymentForm()
-    
-    context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' }
-    
-    cart = request.session.get('cart', {})
-    cart_items_and_total = get_cart_items(cart)
-    
-    context.update(cart_items_and_total)
+    else:
+        order_form = OrderForm()
+        payment_form = MakePaymentForm()
+        
+        context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' }
+        
+        cart = request.session.get('cart', {})
+        cart_items_and_total = get_cart_items(cart)
+        
+        context.update(cart_items_and_total)
     
     return render(request, "checkout/checkout.html", context)
